ed8a664e00efbad036dd8457c7e5efa8abdaa899
edwinmgallego <ing.edwin.gallego@gmail.com>
2022-10-11 16:04:07 -0500

    Initial commit

---
+++ src/main/java/com/example/postgres/springbootpostgressdocker/SpringbootPostgressDockerApplication.java
@@ -0,0 +1,13 @@
+package com.example.postgres.springbootpostgressdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPostgressDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootPostgressDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/postgres/springbootpostgressdocker/SpringbootPostgressDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.postgres.springbootpostgressdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPostgressDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
